c745bc13732e4fb3eb2d5c280a917f9ce49fcabe
Radchenko27 <dimaradchenko270304@yandex.ru>
2026-05-15 16:51:44 +0300

    Исправление null bytes в crud.py и users.py

--- app/routers/api/users.py
+++ app/routers/api/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Header, Response
+﻿from fastapi import APIRouter, Depends, HTTPException, status, Header, Response
 from sqlalchemy.orm import Session
 from typing import List
 from datetime import datetime
